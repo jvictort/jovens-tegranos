9773388eb7e1704019f56dadbdecb07937e4be4a
jvictort <jvteodorobrasil@gmail.com>
2023-03-31 13:02:05 -0300

    Quinto exercício

---
+++ controllers/exercicio-05/exercicio-05.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const circleRadius = parseFloat(form['circle-radius'].value);
+
+  let circleArea = (Math.PI * (circleRadius ** 2)).toLocaleString('pt-br', {minimumFractionDigits: 3});
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>A área da circunferência é de ${circleArea}</p>
+  `
+})
