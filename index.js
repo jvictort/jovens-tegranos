b669bf04f0ae5fdddcee733b50f553357931b067
jvictort <jvteodorobrasil@gmail.com>
2023-03-31 12:52:41 -0300

    Remodelação e componentização do projeto

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const headerContainer = document.querySelector('header');
+// const headerContainer = document.querySelector('header');
 const hamburguerMenu = document.querySelector('.hamburguer-menu');
 const menuLinks = document.querySelectorAll('.menu-container li a[href^="#"]');
 const menuContainer = document.querySelector('.menu-container');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > headerContainer.offsetHeight) {
-      headerContainer.classList.add('active');
-  } else {
-      headerContainer.classList.remove('active');
-  }
-});
 
 hamburguerMenu.addEventListener('click', () => {
   hamburguerMenu.classList.toggle('active');
