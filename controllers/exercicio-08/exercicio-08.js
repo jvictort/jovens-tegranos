e1218d14ade05be50f23ea9ed110129963e8a00f
jvictort <jvteodorobrasil@gmail.com>
2023-03-31 13:03:36 -0300

    Oitavo exercício

---
+++ controllers/exercicio-08/exercicio-08.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const firstMeasure = parseFloat(form['first-measure'].value);
+  const secondMeasure = parseFloat(form['second-measure'].value);
+  const thirdMeasure = parseFloat(form['third-measure'].value);
+
+  let squareArea = (firstMeasure ** 2).toLocaleString('pt-br', {minimumFractionDigits: 4});
+  let triangleArea = ((firstMeasure * secondMeasure) / 2).toLocaleString('pt-br', {minimumFractionDigits: 4});
+  let trapezeArea = (((firstMeasure + secondMeasure) * thirdMeasure) / 2).toLocaleString('pt-br', {minimumFractionDigits: 4})
+
+  // let avarageCost = (distance / spentFuel).toLocaleString('pt-br', {minimumFractionDigits: 3});
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>A área do quadrado é de ${squareArea}m²</p>
+    <p>A área do triângulo é de ${triangleArea}m²</p>
+    <p>A área do trapézio é de ${trapezeArea}m²</p>
+  `
+})
