ff4aaf1e6ff4f673f2b30e72ab0f504ee1fd3dfa
jvictort <jvteodorobrasil@gmail.com>
2023-03-31 13:00:49 -0300

    Segundo exercício

---
+++ controllers/exercicio-02/exercicio-02.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const rectangleBase = parseFloat(form['rectangle-base'].value);
+  const rectangleHeight = parseFloat(form['rectangle-height'].value);
+
+  let rectangleArea = (rectangleBase * rectangleHeight).toLocaleString('pt-br', {minimumFractionDigits: 4});
+  let rectanglePerimeter = ((rectangleBase + rectangleHeight) * 2).toLocaleString('pt-br', {minimumFractionDigits: 4});
+  let rectangleDiagonal = (Math.sqrt((rectangleBase ** 2) + (rectangleHeight ** 2))).toLocaleString('pt-br', {minimumFractionDigits: 4});
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>Área do retângulo: ${rectangleArea}m²</p>
+    <p>Perímetro do retângulo: ${rectanglePerimeter}m</p>
+    <p>Diagonal do retângulo: ${rectangleDiagonal}m</p>
+  `
+})
