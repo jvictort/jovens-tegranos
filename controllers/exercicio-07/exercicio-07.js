d5cb2ef67d48bf32d15097f4d51fd5a52b1a7c5f
jvictort <jvteodorobrasil@gmail.com>
2023-03-31 13:03:21 -0300

    Sétimo exercício

---
+++ controllers/exercicio-07/exercicio-07.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const distance = parseFloat(form['distance'].value);
+  const spentFuel = parseFloat(form['spent-fuel'].value);
+
+  let avarageCost = (distance / spentFuel).toLocaleString('pt-br', {minimumFractionDigits: 3});
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>O consumo médio foi de ${avarageCost}</p>
+  `
+})
