78894167b671630f87000a1c4d286dc7d56b4222
jvictort <jvteodorobrasil@gmail.com>
2023-04-02 05:32:45 -0300

    Décimo nono exercício

---
+++ controllers/exercicio-19/exercicio-19.js
@@ -0,0 +1,56 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const salary  = parseFloat(form['salary'].value);
+
+  let newSalary;
+  let increaseRate;
+  let salaryIncrease;
+
+  /*
+    O exercício especifica da seguinte maneira:
+
+    - Até R$ 999.00 -> 20%
+    - Entre R$ 1000.00 e R$ 2999.00 -> 15%
+    - Entre R$ 3000.00 e R$ 7999.00 -> 10%
+    - Acima de R$ 8000.00 -> 05%
+
+    No entanto não é dito o que fazer no caso de R$ 999.01 ... R$ 999.99. Portanto, tomei como
+    iniciativa estender a taxa de 20% até R$ 999.99.
+
+    O mesmo ocorre para os outros casos.
+  */
+
+  if(salary <= 999.99) {
+    increaseRate = 0.20;
+    salaryIncrease = salary * increaseRate;
+    newSalary = salary + salaryIncrease;
+
+  } else if(salary <= 2999.99) {
+    increaseRate = 0.15;
+    salaryIncrease = salary * increaseRate;
+    newSalary = salary + salaryIncrease;
+
+  } else if(salary <= 7999.99) {
+    increaseRate = 0.10;
+    salaryIncrease = salary * increaseRate;
+    newSalary = salary + salaryIncrease;
+
+  } else {
+    increaseRate = 0.05;
+    salaryIncrease = salary * increaseRate;
+    newSalary = salary + salaryIncrease;
+  }
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>Novo salário: ${newSalary.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</p>
+    <p>Aumento: ${salaryIncrease.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</p>
+    <p>Porcentagem de aumento: ${increaseRate.toLocaleString('pt-br', {style: 'percent'})}</p>
+  `
+})
