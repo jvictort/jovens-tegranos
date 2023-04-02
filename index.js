29945ef688edc567ff0bc275f7792e215f3b1099
jvictort <jvteodorobrasil@gmail.com>
2023-04-02 16:45:59 -0300

    Responsividade

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-// const headerContainer = document.querySelector('header');
 const hamburguerMenu = document.querySelector('.hamburguer-menu');
 const menuLinks = document.querySelectorAll('.menu-container li a[href^="#"]');
 const menuContainer = document.querySelector('.menu-container');
+
+const linkDropdown = document.querySelectorAll('.list-dropdown');
+
+linkDropdown.forEach(link => {
+  link.addEventListener('mouseover', event => {
+    const formField = document.querySelectorAll('.form-fields');
+
+    formField.forEach(field => {
+      field.style.position = 'static';
+    });
+  });
+
+  link.addEventListener('mouseout', event => {
+    const formField = document.querySelectorAll('.form-fields');
+
+    formField.forEach(field => {
+      field.style.position = 'relative';
+      });
+    });
+});
 
 hamburguerMenu.addEventListener('click', () => {
   hamburguerMenu.classList.toggle('active');
