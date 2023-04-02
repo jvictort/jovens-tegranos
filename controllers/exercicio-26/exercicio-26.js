dcb928c2b1162f96aa612d92ef7e59b18b95afbf
jvictort <jvteodorobrasil@gmail.com>
2023-04-02 05:36:07 -0300

    Vigésimo sexto exercício

---
+++ controllers/exercicio-26/exercicio-26.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const vowelsList = ['a', 'e', 'i', 'o', 'u'];
+
+  const word = form['word'].value;
+
+  let vowelNumber = 0;
+  let consonantNumber = 0;
+
+  letters = word.split('');
+
+  letters.forEach(letter => {
+    if(vowelsList.includes(letter)) {
+      vowelNumber++;
+    } else {
+      consonantNumber++;
+    }
+  });
+
+  contentTitle.textContent = 'Resultado';
+
+  contentResult.innerHTML = `
+    <p>O número de vogais é: ${vowelNumber}</p>
+    <p>O número de consoantes é: ${consonantNumber}</p>
+  `
+
+})
