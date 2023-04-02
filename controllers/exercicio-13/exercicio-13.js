30efce139a4ef362f3f9cb56ab4ab4970e2caac2
jvictort <jvteodorobrasil@gmail.com>
2023-04-02 05:31:02 -0300

    Décimo terceiro exercício

---
+++ controllers/exercicio-13/exercicio-13.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const minutes  = parseInt(form['minutes'].value);
+ 
+  let chargeAmount = 50;
+
+  if(minutes > 100) {
+    let addedMinutes = minutes - 100;
+
+    chargeAmount += addedMinutes * 2; 
+  }
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>A quantidade a ser paga é de ${chargeAmount.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</p>
+  `
+})
