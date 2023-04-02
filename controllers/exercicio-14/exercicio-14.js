b4148292632438c691d0a619ba64769b857dece1
jvictort <jvteodorobrasil@gmail.com>
2023-04-02 05:31:21 -0300

    Décimo quarto exercício

---
+++ controllers/exercicio-14/exercicio-14.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const unitPrice = parseFloat(form['unit-price'].value);
+  const productQuantify = parseInt(form['product-quantify'].value);
+  const moneyAmount = parseFloat(form['money-amount'].value);
+
+  let totalPrice = productQuantify * unitPrice;
+  let changePrice = (moneyAmount - totalPrice);
+
+  let message = changePrice < 0 ?
+      `Dinheiro insuficiente. Faltam ${(changePrice * -1).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}` :
+      `O troco será de ${changePrice.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
+
+  contentTitle.textContent = 'Resultado';
+  contentResult.innerHTML = `
+    <p>${message}</p>
+  `
+})
