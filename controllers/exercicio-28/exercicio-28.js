2729902736426668000222226c281582f498de26
jvictort <jvteodorobrasil@gmail.com>
2023-04-02 05:36:47 -0300

    Vigésimo oitavo exercício

---
+++ controllers/exercicio-28/exercicio-28.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+
+const contentTitle = document.querySelector('#content-title');
+const contentResult = document.querySelector('#content-result');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const inputNumbers = document.querySelectorAll('input');
+
+  let numbers = [];
+  let sortedNumbers = '';
+
+  inputNumbers.forEach(inputNumber => {
+    numbers.push(parseFloat(inputNumber.value));
+  })
+
+  let maxNumber = Math.max(...numbers).toLocaleString('pt-br');
+  let minNumber = Math.min(...numbers).toLocaleString('pt-br');
+
+  contentTitle.textContent = 'Resultado';
+
+  contentResult.innerHTML = `
+    <p>O maior número é ${maxNumber} e o menor número é ${minNumber}</p>
+  `
+
+})
